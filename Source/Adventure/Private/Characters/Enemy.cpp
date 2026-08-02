5a6beb8b46a63a8cea3c26b483671c3fe7cc7131
moha-mmadamin <ma2005.akbari@gmail.com>
2026-08-02 22:24:32 +0330

    Create BP_Enemy and its c++ code
    import character from asset as being enemy then BP_Enemy ABP_Enemy is
    created after that import suitable animation for unarmed possition from
    inside of asset into ABP_Enemy Locomotion section

---
+++ Source/Adventure/Private/Characters/Enemy.cpp
@@ -0,0 +1,26 @@
+#include "Characters/Enemy.h"
+
+AEnemy::AEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
